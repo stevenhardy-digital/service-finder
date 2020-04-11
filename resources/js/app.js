fb75e2f59060907b64c143c4fa1c5216d6a88f30
medicsteven <steven.hardy26@hotmail.co.uk>
2020-04-11 06:50:31 +0100

    add google maps package. centre map on click

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,14 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
+import * as VueGoogleMaps from "vue2-google-maps";
+
+Vue.use(VueGoogleMaps, {
+    load: {
+        key: process.env.MIX_GOOGLE_API
+    }
+});
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -26,6 +34,8 @@ files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+
+
 
 const app = new Vue({
     el: '#app',
--- resources/js/components/HomeComponent.vue
+++ resources/js/components/HomeComponent.vue
@@ -2,7 +2,7 @@
     <div class="main-container">
         <div class="row no-gutters">
             <div class="col-sm-4 services-list">
-               <div class="location" v-for="location in locations" :key="location.id">
+               <div class="location" v-for="location in locations" :key="location.id" @click="centreMap(location)">
                    <div class="logo">
                        <img :src="location.logo" :alt="location.name" />
                    </div>
@@ -14,51 +14,34 @@
                </div>
             </div>
             <div class="col-sm-8 map">
-                <GoogleMapLoader
-                    :mapConfig="mapConfig"
-                >
-                    <template slot-scope="{ google, map }">
-  	                    <GoogleMapMarker
-                            v-for="marker in markers"
-                            :key="marker.id"
-                            :marker="marker"
-                            :google="google"
-                            :map="map"
-                        />
-                        <GoogleMapLine
-                            v-for="line in lines"
-                            :key="line.id"
-                            :path.sync="line.path"
-                            :google="google"
-                            :map="map"
-                        />
-                    </template>
-                </GoogleMapLoader>
+               <GmapMap
+                    :center="center"
+                    :zoom="13"
+                    map-type-id="terrain"
+                    class="google-map"
+                    >
+                    <GmapMarker
+                        :key="index"
+                        v-for="(m, index) in markers"
+                        :position="m.position"
+                        :clickable="true"
+                        :draggable="true"
+                        @click="center=m.position"
+                    />
+                </GmapMap>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import GoogleMapLoader from './GoogleMapsComponent'
-    import GoogleMapMarker from './GoogleMapMarker'
-    import GoogleMapLine from './GoogleMapLine'
-    import { mapSettings } from '../constants/mapSettings'
-
     export default {
-        components: {
-            GoogleMapLoader,
-            GoogleMapMarker,
-            GoogleMapLine
-        },
-
         data() {
             return {
                 locations: [],
-                markers: [
-                    { id: '1', position: { lat: 51.880087, lng: 0.550927 } }
-                ],
-                lines: []
+                markers: [],
+                lines: [],
+                center: {lat:51.880087, lng:0.550927}
             }
         },
         
@@ -77,10 +60,8 @@
 
         methods: {
             getMarkers() {
-                console.log('getting')
                 const result = this.locations.map((item) => {
                     return {
-                        id: item.id,
                         position: {
                             lat: Number(item.latitude),
                             lng: Number(item.longitude)
@@ -89,20 +70,17 @@
                 });
 
                 this.markers = result;
+            },
+
+            centreMap(location) {
+                const result =  {
+                        lat: Number(location.latitude),
+                        lng: Number(location.longitude)
+                };
+
+                this.center = result;
             }
         },
-
-        computed: {
-            mapConfig () {
-                return {
-                    ...mapSettings,
-                    center: this.mapCenter
-                }
-            },
-            mapCenter () {
-                return this.markers[0].position
-            }
-        }
     }
 </script>
 
