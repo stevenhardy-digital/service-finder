592a4da75287d0b9fe9be413372f27d857ab1f74
medicsteven <steven.hardy26@hotmail.co.uk>
2020-04-13 20:43:59 +0100

    Disable register

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes([
+    'register' => false
+]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/add-location', 'LocationController@index' )->name('add-location');
